2310b223e94b08d19e755cfe5c0f45986468deb3
hy <1155090@qq.com>
2017-11-30 11:20:52 +0800

    试试码云

--- Day03/函数.py
+++ Day03/函数.py
@@ -1,3 +1,19 @@
 #!/user/bin/env python
 # __author__ = 'Howie'
 # date : 2017/11/30
+
+
+def information(gender="man", *args, **kwargs):
+    print(
+        '''
+        name:%s
+        age:%d
+        job:%s
+        sal:%d
+        address:%s
+        gender:%s
+        ''' % (kwargs["name"], args[0], kwargs["job"], args[1], kwargs["address"], gender)
+    )
+
+
+information("female", 30, 12000, name="hy", job="IT", address="GZ")
